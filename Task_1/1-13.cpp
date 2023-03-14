e5064b989580116a8fda8c41cc4c4fa1fcd5e924
RDindaclub <RDindaclub@gmail.com>
2023-03-14 19:06:49 +0300

    first commit

---
+++ Task_1/1-13.cpp
@@ -0,0 +1,43 @@
+/*
+Дан массив размера N. Найти два соседних элемента, сумма которых максимальна, и вывести
+эти элементы в порядке возрастания их индексов.
+*/
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int N, isum;
+    cout << "N = ";
+    cin >> N;
+    int *str = new int[N];
+    int sum = -100;
+    for (int i = 0; i < N; i++)
+    {
+        cout << "str(" << i + 1 << ") - ";
+        cin >> str[i];
+    }
+    for (int i = 0; i < N - 1; i++)
+    {
+        if (str[i] + str[i + 1] > sum)
+        {
+            sum = str[i] + str[i + 1];
+            isum = i;
+        }
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << str[i] << " ";
+    }
+    cout << endl;
+
+    cout << isum << " - " << str[isum] << endl
+         << isum + 1 << " - " << str[isum + 1] << endl
+         << "sum = " << sum << endl;
+
+    return 0;
+}
